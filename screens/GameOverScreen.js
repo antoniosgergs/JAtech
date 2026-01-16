3606a9e96f14bea227ab91aff4e0fac9b51fe02f
toto <antonios.gerges4@gmail.com>
2026-01-16 15:32:58 +0200

    feat: adding a new button for starting and reset a new game style: adding an image for game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,19 +1,56 @@
-import {Text, StyleSheet} from "react-native";
+import {View, Image, Text,  StyleSheet} from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
-function GameOverScreen() {
+function GameOverScreen({ roundsNumber, userNumber, onStartNewGame  }) {
 return (
-        <Text style={styles.gameOverText}> Game is over!</Text>
+        <View style={styles.rootContainer}>
+           <Title> Game Over!</Title>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image}
+                       source={require("../assets/images/success.png")} />
+            </View>
+                <Text style={styles.summaryText}>
+                    Tou need <Text style={styles.highlightText}>{roundsNumber}</Text> rounds to guess the number <Text style={styles.highlightText}>{userNumber}</Text>.
+                </Text>
+            <PrimaryButton onPress={onStartNewGame}>
+                 Start New Game
+            </PrimaryButton>
+        </View>
 )
 }
 
 export default GameOverScreen;
 
 const styles = StyleSheet.create({
-    gameOverText:{
-        flex: 1,
-        fontSize: 20,
-        fontWeight: "bold",
-        color: "yellow",
-        alignItems: "center",
+   rootContainer: {
+       flex: 1,
+       padding:12,
+       justifyContent:"center",
+       alignItems:"center",
+   },
+    imageContainer:{
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: "hidden",
+        margin:36
+    },
+    image:{
+        width: "100%",
+        height: "100%",
+    },
+    summaryText:{
+       fontFamily:'open-sans',
+        fontSize:28,
+        textAlign:'center',
+        marginVertical:25,
+    },
+    highlightText:{
+       fontFamily:'open-sans-light',
+        color:Colors.primary500,
     }
 })
